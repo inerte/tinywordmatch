b4ad91687c312376174bf8224588f89f8298313d
Julio Nobrega Netto <julionn@yahoo-inc.com>
2015-09-06 17:53:32 -0700

    Add Related Words view on Storyboaord with single Label

--- Tiny Word Match/ViewController.swift
+++ Tiny Word Match/ViewController.swift
@@ -9,17 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    @IBOutlet weak var word: UITextField!
+    
+    @IBAction func startButton(sender: UIButton) {
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
