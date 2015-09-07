5b4643ce96819e87fce76ab37ecc48659cad0a7e
Julio Nobrega Netto <julionn@yahoo-inc.com>
2015-09-07 16:40:19 -0700

    Initial Wolphram Alpha integration, simple parse its XML response

--- Tiny Word Match/WordListViewController.swift
+++ Tiny Word Match/WordListViewController.swift
@@ -8,14 +8,32 @@
 
 import UIKit
 
-class WordListViewController: UIViewController {
+class WordListViewController: UIViewController, NSXMLParserDelegate {
     var toPass:String!
     
+    
     @IBOutlet weak var chosenWord: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         chosenWord.text = toPass
+        
+        let url = "http://api.wolframalpha.com/v2/query?input=cat+rhyme&appid=75JR3G-A8R54WGRH6"
+        
+        // create the request & response
+        var request = NSMutableURLRequest(URL: NSURL(string: url)!, cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalCacheData, timeoutInterval: 5)
+        var response: NSURLResponse?
+        var error: NSError?
+        
+        request.HTTPMethod = "GET"
+        // send the request
+        let data: NSData = NSURLConnection.sendSynchronousRequest(request, returningResponse: &response, error: &error)!
+        
+        let xml = SWXMLHash.parse(data)
+        
+        println("queryresult.pod.withAttr")
+        println(xml["queryresult"]["pod"].withAttr("title", "Result")["subpod"]["plaintext"].element?.text)
     }
 }
